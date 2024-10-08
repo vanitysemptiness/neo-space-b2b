1bc58a213a8027036098e8fac7aa7188e4425c5d
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-10-08 16:46:04 -0400

    feat: enhanced color palette

--- src/components/ColorPickerButton.js
+++ src/components/ColorPickerButton.js
@@ -15,9 +15,47 @@ const ColorPickerButton = () => {
       themeMode: 'light',
       formatToggle: true,
       closeButton: true,
-      clearButton: true,
+      clearButton: false,
       swatches: [
-        '#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#FFFFFF', '#000000'
+        '#FF0000', // Red
+        '#FF4500', // Orange
+        '#FFFF00', // Yellow
+        '#00FF00', // Lime
+        '#00BFFF', // Sky Blue
+        '#0000FF', // Blue
+        '#8B00FF', // Purple
+        '#FF00FF', // Magenta
+        '#FFFFFF', // White
+        '#F5F5F5', // Soft White
+        '#FFFAF0', // Ivory
+        '#FAF0E6', // Linen
+        '#FDF5E6', // Old Lace
+        '#FFFACD', // Lemon Chiffon
+        '#E0FFFF', // Light Cyan
+        '#B0E0E6', // Powder Blue
+        '#ADD8E6', // Light Blue
+        '#87CEEB', // Sky Blue
+        '#87CEFA', // Light Sky Blue
+        '#00CED1', // Dark Turquoise
+        '#40E0D0', // Turquoise
+        '#48D1CC', // Medium Turquoise
+        '#00FA9A', // Medium Spring Green
+        '#00FF7F', // Spring Green
+        '#7CFC00', // Lawn Green
+        '#ADFF2F', // Green-Yellow
+        '#FFD700', // Gold
+        '#DAA520', // Goldenrod
+        '#CD853F', // Peru
+        '#D2691E', // Chocolate
+        '#8B4513', // Saddle Brown
+        '#A0522D', // Sienna
+        '#800080', // Purple
+        '#663399', // Rebecca Purple
+        '#4B0082', // Indigo
+        '#9370DB', // Medium Purple
+        '#BA55D3', // Medium Orchid
+        '#DA70D6', // Orchid
+        '#EE82EE'  // Violet
       ],
       inline: false,
       margin: 10,
@@ -51,9 +89,9 @@ const ColorPickerButton = () => {
         style={{ display: 'none' }}
         onChange={(e) => changeColor(e.target.value)}
       />
-      <button 
+      <button
         onClick={handleButtonClick}
-        style={{ 
+        style={{
           backgroundColor: currentColor,
           width: '30px',
           height: '30px',
